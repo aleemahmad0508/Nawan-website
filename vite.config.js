ae7a124d0084a4317cb251711b4a31cf00eef5e4
aleemahmad0508 <aleem.ahmad0508@gmail.com>
2026-09-03 21:32:17 +0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/Nawan-website/', // Added this line matching your repository name
+   // Added this line matching your repository name
 })

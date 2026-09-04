fa315a88ea33b567c90c09c5e60cb9dfc8b6bd1d
aleemahmad0508 <aleem.ahmad0508@gmail.com>
2026-09-04 07:13:11 +0500

    fixed again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/Nawan-website/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/Nawan-website/'
+    : '/',
    // Added this line matching your repository name
 })
